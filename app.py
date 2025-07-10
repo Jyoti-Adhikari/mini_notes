bf76dfcae2e91e9d4888d19d6a45804df545a2db
Jyoti-Adhikari <jyotiadhikari770@gmail.com>
2025-07-10 19:58:05 +0545

    mini notes

--- app.py
+++ app.py
@@ -76,5 +76,9 @@ def logout():
     session.clear()
     return redirect('/login')
 
+import os
+
 if __name__ == '__main__':
-    app.run()
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
+    
